e8052bbb01f380c8a2dd2ab66c41ae66b245f6e1
fjmartinf01 <fjmartinf01@aula1-04>
2015-10-07 19:45:52 +0200

    Subo primer ejercicio de Java

---
+++ Entornodedesarrollo/src/entornodedesarrollo/Entornodedesarrollo.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entornodedesarrollo;
+
+/**
+ *
+ * @author fjmartinf01
+ */
+public class Entornodedesarrollo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    int numero1= 1;
+    int numero2= 2;
+    int suma;
+    suma=numero1+numero2;
+        System.out.println("El número es: " + suma);
+    }
+    
+}
